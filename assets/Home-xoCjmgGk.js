d50c400aca187072aab610a08d92ec58bcf303de
rakshit-singh1 <thisisjames007@gmail.com>
2025-01-08 15:02:39 +0530

    portfolio

---
+++ assets/Home-xoCjmgGk.js
@@ -0,0 +1,1 @@
+import{j as o,T as e}from"./index-D__zgqP4.js";import{C as t}from"./Container-DR3LUNw5.js";const s=()=>o.jsxs(t,{children:[o.jsx(e,{variant:"h4",gutterBottom:!0,children:"Welcome to My Portfolio"}),o.jsx(e,{children:"I am a Blockchain and React.js developer with expertise in building web3 solutions."})]});export{s as default};
